a74b4870ce6c35ebcc242e78c45ab53ff417666d
huca06001 <huca06001@E-PW05C37R>
2024-12-11 12:04:57 +0100

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 11; i++)
+                Console.WriteLine(i);
+        }
+    }
+}
